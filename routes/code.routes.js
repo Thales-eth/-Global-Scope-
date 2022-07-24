c500101738a3e4d284d64a1554475de11fbb4c2b
Thales <dan.console.log@gmail.com>
2022-07-24 19:46:48 +0200

    writefilesync

--- routes/code.routes.js
+++ routes/code.routes.js
@@ -1,9 +1,29 @@
 const router = require("express").Router()
+const exec = require('child_process').exec;
+const { text } = require("express");
 const fs = require('fs')
 
 router.post('/js', (req, res) => {
-    fs.writeFileSync('test.js', req.body.code)
-    res.json({ message: 'success' })
+
+    // 1st try
+    fs.writeFileSync('sum.js', req.body.code)
+
+    // const funCode = exec('npm test',
+    //     (error, stdout, stderr) => {
+    //         console.log(`stdout: ${stdout}`);
+    //         console.log(`stderr: ${stderr}`);
+    //         if (error !== null) {
+    //             console.log(`exec error: ${error}`);
+    //         }
+    //         res.json({ results: stderr })
+    //     });
+
+    // console.log(typeof exec)
+
+    // fs.writeFile('sum.js', req.body.code, funCode)
+
 })
 
 module.exports = router
+
+
