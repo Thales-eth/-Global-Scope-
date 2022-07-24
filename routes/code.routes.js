923eb0726f6a10afebe9e20ed91773c65cc6dc5a
Víctor <victorsobrinos@gmail.com>
2022-07-24 19:48:24 +0200

    routes checked

--- routes/code.routes.js
+++ routes/code.routes.js
@@ -1,9 +1,14 @@
 const router = require("express").Router()
+const { exec } = require('node:child_process');
+
 const fs = require('fs')
 
 router.post('/js', (req, res) => {
     fs.writeFileSync('test.js', req.body.code)
     res.json({ message: 'success' })
 })
+
+
+exec('"./../test.js" 2 4');
 
 module.exports = router
