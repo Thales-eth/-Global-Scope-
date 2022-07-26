a84e5c384b7febe2b7a751a0bcc975bb486cb6a9
Víctor <victorsobrinos@gmail.com>
2022-07-26 09:35:41 +0200

    katas code clean

--- katas/001/kata.js
+++ katas/001/kata.js
@@ -1,6 +1,5 @@
 function sum(a,b) { 
- //write your code here
-  return a+b
- }
- // Do not touch anything below this line :3
- module.exports = sum
+//write your code here
+}
+// Do not touch anything below this line :3
+module.exports = sum
