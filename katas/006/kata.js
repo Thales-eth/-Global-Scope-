c32cce167b8e6e39aca47e22cad068750c8d99ad
Thales <dan.console.log@gmail.com>
2022-07-28 10:14:46 +0200

    kata local changes

--- katas/006/kata.js
+++ katas/006/kata.js
@@ -1,7 +1,7 @@
 function isSquare(int) { 
  //write your code here
-  let num = Math.sqrt(int)
-  return num % 1 === 0
- }
+let num = Math.sqrt(int)
+return num % 1 === 0 ? true : false   
+}
  // Do not touch anything below this line :3
  module.exports = isSquare
